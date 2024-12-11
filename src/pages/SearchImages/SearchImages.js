0841a66b3f0ba0d26bdf5d9016494802f44d2883
Iñigo Montánchez <mci.m89@gmail.com>
2024-12-11 12:14:53 +0100

    feat: create the different pages, as Main, SearchText, SearchImages and Graph. Create a component Navbar to test the route behavior.

---
+++ src/pages/SearchImages/SearchImages.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SearchImages = () => {
+    return (
+        <div>
+            <h1>Pantalla de Búsqueda de Imagenes</h1>
+        </div>
+    );
+};
+
+export default SearchImages;
