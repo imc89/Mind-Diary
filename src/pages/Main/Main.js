e529e918fdca1195a953355cbee2bc362e5e2069
Iñigo Montánchez <mci.m89@gmail.com>
2024-12-11 14:37:13 +0100

    feat: Calendar and its styles

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Calendar from 'react-calendar';
+import './Main.css';
 
 const Main = () => {
+    const [date, setDate] = useState(new Date());
+
+    const onDateChange = (newDate) => {
+        setDate(newDate);
+    };
+
     return (
         <div>
-            <h1>Pantalla Principal</h1>
+            <div className="header">
+                <h1>Mi Diario Interactivo</h1>
+            </div>
+            <Calendar
+                onChange={onDateChange}
+                value={date}
+                showNeighboringMonth={false}
+            />
         </div>
     );
 };
