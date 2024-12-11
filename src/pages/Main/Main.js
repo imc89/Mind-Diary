9464406019dceab1a7eb578d1643f4ef17d47c4e
Iñigo Montánchez <mci.m89@gmail.com>
2024-12-11 15:44:56 +0100

    feat: try to solve the focus effect in ios

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Calendar from 'react-calendar';
 import './Main.css';
 
@@ -9,6 +9,16 @@ const Main = () => {
         setDate(newDate);
     };
 
+   
+        useEffect(() => {
+          // Asegúrate de que el código se ejecute después de que los elementos estén en el DOM
+          const tiles = document.querySelectorAll('.react-calendar__tile');
+          tiles.forEach((tile) => {
+            tile.setAttribute('tabindex', '0');
+          });
+        }, []); // Ejecutar solo una vez al montar el componente
+      
+      
     return (
         <div>
             <div className="header">
