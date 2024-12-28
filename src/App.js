6843681572dc20856053ce68faa40acc4de84069
Iñigo Montánchez <mci.m89@gmail.com>
2024-12-28 16:39:18 +0100

    feat: creating and using component SplashScreen.(v1.0)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// Pages
+// PAGES
 import Main from './pages/Main/Main';
 import Search from './pages/SearchText/SearchText';
 import Images from './pages/SearchImages/SearchImages';
 import Graph from './pages/Graph/Graph';
-// Components
+// COMPONENTS
 import Navbar from './components/Navbar';
+import SplashScreen from './components/SplashScreen/SplashScreen';
 
 function App() {
+    // STATE TO SHOW/HIDE THE SPLASHSCREEN
+    // ESTADO PARA MOSTRAR/OCULTAR EL SPLASHSCREEN
+    const [showSplash, setShowSplash] = useState(true);
+
+    // METHOD TO GET OUT OF SPLASHSCREEN
+    // METODO PARA SALIR DEL SPLASHSCREEN
+    const handleSplashFinish = () => {
+        setShowSplash(false);
+    };
+
     return (
         <Router basename="/Mind-Diary">
-            <Navbar />
-            <Routes>
-                <Route path="/" element={<Main />} />
-                <Route path="/search" element={<Search />} />
-                <Route path="/image" element={<Images />} />
-                <Route path="/graph" element={<Graph />} />
-            </Routes>
+            {showSplash ? (
+                <SplashScreen onFinish={handleSplashFinish} />
+            ) : (
+                <>
+                    <Navbar />
+                    <Routes>
+                        <Route path="/" element={<Main />} />
+                        <Route path="/search" element={<Search />} />
+                        <Route path="/image" element={<Images />} />
+                        <Route path="/graph" element={<Graph />} />
+                    </Routes>
+                </>
+            )}
         </Router>
     );
 }
